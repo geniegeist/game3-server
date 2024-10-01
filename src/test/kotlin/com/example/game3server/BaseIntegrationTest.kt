60534b8196ae045d52c627faef82e555d6db4992
Duc Nguyen <vduc@fastmail.com>
2024-10-01 15:50:16 +0200

    Add integration test for GameService

--- src/test/kotlin/com/example/game3server/BaseIntegrationTest.kt
+++ src/test/kotlin/com/example/game3server/BaseIntegrationTest.kt
@@ -4,9 +4,8 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
-import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.DockerImageName
+import org.testcontainers.redpanda.RedpandaContainer
 
 @SpringBootTest
 @Testcontainers
@@ -14,17 +13,26 @@ import org.testcontainers.utility.DockerImageName
 class BaseIntegrationTest {
 
     companion object {
-        private val kafkaContainer: KafkaContainer =
-            KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))
+        // private val kafkaContainer: KafkaContainer =
+        //     KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka"))
+        //         .apply {
+        //             withNetwork(network)
+        //             withExposedPorts(9093)
+        //             start()
+        //         }
+        val redPandaContainer =
+            RedpandaContainer("docker.redpanda.com/redpandadata/redpanda")
                 .apply {
-                    withExposedPorts(9093)
+                    withExposedPorts(8081, 9091, 9092)
                     start()
                 }
+
 
         @JvmStatic
         @DynamicPropertySource
         private fun registerProperties(registry: DynamicPropertyRegistry) {
-            registry.add("kafka.bootstrapServers") { kafkaContainer.bootstrapServers }
+            registry.add("kafka.bootstrapServers") { redPandaContainer.bootstrapServers }
+            registry.add("kafka.schemaRegistryUrl") { redPandaContainer.schemaRegistryAddress }
             registry.add("game.service.auto-start") { false }
         }
     }
--- src/test/kotlin/com/example/game3server/application/GameServiceTest.kt
+++ src/test/kotlin/com/example/game3server/application/GameServiceTest.kt
@@ -10,53 +10,106 @@ import com.example.game3server.infrastructure.messaging.consumption.GameLogKafka
 import com.example.game3server.infrastructure.messaging.production.GameLogKafkaSender
 import com.example.game3server.infrastructure.messaging.production.ServerCommandKafkaSender
 import com.ninjasquad.springmockk.MockkBean
+import game3.events.client.command.AuthenticateCommand
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig
 import io.mockk.every
+import org.apache.avro.specific.SpecificRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.InjectMocks
 import org.mockito.MockitoAnnotations
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationEventPublisher
+import org.springframework.test.annotation.DirtiesContext
 import scala.reflect.internal.TreeInfo.Applied
+import java.util.*
 
-internal class GameServiceTest : BaseIntegrationTest() {
+internal class GameServiceTest(
+    @Value("\${kafka.bootstrapServers}") private val bootstrapServers: String,
+    @Value("\${kafka.schemaRegistryUrl}") private val schemaRegistryUrl: String
+) : BaseIntegrationTest() {
 
-    @MockkBean(relaxed = true)
-    lateinit var gameSnapshotRepository: GameSnapshotRepository<Long>
+    // @MockkBean(relaxed = true)
+    // lateinit var gameSnapshotRepository: GameSnapshotRepository<Long>
 
-    @MockkBean
-    lateinit var publisher: ApplicationEventPublisher
+    // @MockkBean
+    // lateinit var publisher: ApplicationEventPublisher
 
-    @MockkBean
-    lateinit var gameLogConsumable: GameLogConsumable
+    // @MockkBean
+    // lateinit var gameLogConsumable: GameLogConsumable
 
-    //@MockkBean(relaxed = true)
-    //lateinit var gameLogListener: GameLogKafkaListener
+    // //@MockkBean(relaxed = true)
+    // //lateinit var gameLogListener: GameLogKafkaListener
 
-    //@MockkBean(relaxed = true)
-    lateinit var clientCommandListener: ClientCommandKafkaListener
+    // //@MockkBean(relaxed = true)
+    // lateinit var clientCommandListener: ClientCommandKafkaListener
 
-    @MockkBean
-    lateinit var serverCommandSender: ServerCommandKafkaSender
+    // @MockkBean
+    // lateinit var serverCommandSender: ServerCommandKafkaSender
 
-    @MockkBean
-    lateinit var gameLogSender: GameLogKafkaSender
+    // @MockkBean
+    // lateinit var gameLogSender: GameLogKafkaSender
 
     @Autowired
     lateinit var gameService: GameService
 
+    // Support val
+    private val clientCommandProducer = KafkaProducer<String, SpecificRecord>(kafkaProducerProps())
+    private val gameLogConsumer = KafkaConsumer<String, SpecificRecord>(kafkaConsumerProps(topic = GAME_LOG_TOPIC))
+
+    companion object {
+        const val CLIENT_COMMAND_TOPIC = "game3.events.client.command"
+        const val GAME_LOG_TOPIC = "game3.events.game.log"
+    }
+
     @Test
+    @DirtiesContext
     fun `write a InitGameMove to the game log`() {
+        // GIVEN:
+        // - empty game log
+        // as well as:
+        val senderId = "playerA"
+        clientCommandProducer.send(ProducerRecord(CLIENT_COMMAND_TOPIC, senderId, AuthenticateCommand(senderId)))
 
-        every { gameSnapshotRepository.get() }.returns(
-            GameSnapshot<Long>(
-                Game.createEmpty(),
-                0
-            )
-        )
-
+        // WHEN
         gameService.start()
 
+        // THEN
+
+    }
+
+    private fun kafkaConsumerProps(topic: String? = null, enableAutoCommit: Boolean = true): Map<String, Any> {
+        val props = mutableMapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to "org.apache.kafka.common.serialization.StringDeserializer",
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to "io.confluent.kafka.serializers.KafkaAvroDeserializer",
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to enableAutoCommit,
+            ConsumerConfig.GROUP_ID_CONFIG to UUID.randomUUID().toString(),
+            KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG to redPandaContainer.schemaRegistryAddress,
+            KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG to true,
+        )
+
+        if (topic != null) {
+            props[ConsumerConfig.GROUP_ID_CONFIG] = topic
+        }
+
+        return props
+    }
+
+    private fun kafkaProducerProps(): Properties {
+        val props = Properties()
+        props["bootstrap.servers"] = bootstrapServers
+        props["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+        props["value.serializer"] = "io.confluent.kafka.serializers.KafkaAvroSerializer"
+        props["schema.registry.url"] = schemaRegistryUrl
+        props[KafkaAvroSerializerConfig.VALUE_SUBJECT_NAME_STRATEGY] = "io.confluent.kafka.serializers.subject.TopicRecordNameStrategy"
+        return props
     }
 }
